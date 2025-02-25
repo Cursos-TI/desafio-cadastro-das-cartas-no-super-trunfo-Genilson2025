9874b0e9f578a0b6eb2a28d5ceef4b17b95a0b73
Genilson2025 <grb.engenharia@hotmail.com>
2025-02-25 15:47:50 +0000

    pib

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,7 @@ int main() {
    char codigo01[20] , codigo02[20];
    char cidade01[50] , cidade02[20];
    int populacao01 , populacao02;
-   float pip01 , pip02;
+   float pib01 , pib02;
    float area01 , area02;
    int pontosturisticos01 , pontosturistcos02;
 
@@ -15,8 +15,24 @@ int main() {
    printf("Digite as infomações da carta 01 \nEstado: ");
    scanf(" %s", estado01);
 
-   printf("Código da carta \ncodigo01");
-   scanf("%d" , codigo01);
+   printf("Código da carta: ");
+   scanf(" %s" , codigo01);
+
+   printf("Nome da cidade: ");
+   scanf("%s" , cidade01);
+
+   printf("População: ");
+   scanf("%d", &populacao01);
+
+   printf("Área: ");
+   scanf("%f" , &area01);
+
+   printf("Pib: ");
+   scanf("%f" , &pib01);
+
+
+
+
 
    
 

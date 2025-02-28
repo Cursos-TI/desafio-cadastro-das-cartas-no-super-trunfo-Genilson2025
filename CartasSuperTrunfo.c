4dcfc8c62ece76efa9e29ae7cc6d969daa7d14d8
Genilson2025 <grb.engenharia@hotmail.com>
2025-02-28 15:25:51 -0300

    termino

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,17 +3,20 @@
 
 
 int main() {
+       //inserindo variaveis de carta 1 e 2.
+
    char estado01[50] , estado02[50];
    char codigo01[50] , codigo02[50];
-   char cidade01[50] , cidade02[20];
+   char cidade01[50] , cidade02[50];
    int populacao01 , populacao02;
    float pib01 , pib02;
    float area01 , area02;
-   int pontosturisticos01 , pontosturistcos02;
+   int pontosturisticos01 , pontosturisticos02;
 
+     //solicitar informações de carta de numero 1.
        
    printf("Digite as infomações da carta 01 \nEstado: ");
-   scanf(" %s", estado01);
+   scanf("%s", estado01);
 
    printf("Código da carta: ");
    scanf(" %s" , codigo01);
@@ -30,22 +33,40 @@ int main() {
    printf("Pib: ");
    scanf("%f" , &pib01);
 
-   printf("Numero de pontos turistico ");
-   scanf("%d" , pontosturisticos01);
-   
+   printf("Numero de pontos turistico: ");
+   scanf("%d" , &pontosturisticos01);
+
+    //solicitar informações de carta de numero 02.
+
+    printf("Digite as infomações da carta 02 \nEstado: ");
+    scanf(" %s", estado02);
+ 
+    printf("Código da carta: ");
+    scanf(" %s" , codigo02);
+ 
+    printf("Nome da cidade: ");
+    scanf("%s" , cidade02);
+ 
+    printf("População: ");
+    scanf("%d", &populacao02);
+ 
+    printf("Área: ");
+    scanf("%f" , &area02);
+ 
+    printf("Pib: ");
+    scanf("%f" , &pib02);
+ 
+    printf("Numero de pontos turistico: ");
+    scanf("%d" , &pontosturisticos02);
+  
+
+     //imprimindo carta de numero 01 e 02.
+
+    printf("Carta01: \n Estado: %s\n Código da carta: %s\n Nome da cidade: %s\n População: %d\n Àrea: %f Km\n Pib: %f bilhões de reais\n Pontos turisticos: %d\n" , estado01 , codigo01 , cidade01 , populacao01 , area01 , pib01 , pontosturisticos01 );
 
 
+    printf("Carta02: \n Estado: %s\n Código da carta: %s\n Nome da cidade: %s\n População: %d\n Àrea: %f Km\n Pib: %f bilhões de reais\n Pontos turisticos: %d\n" , estado02 , codigo02 , cidade02 , pontosturisticos02 , area02 , pib02 , pontosturisticos02);
 
+return 0;
 
-
-   
-
-
-
-
-
-
-
-
-    return 0;
 }
